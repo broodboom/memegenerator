636748b988ea2f027d4417215516e734c9d8e223
Dave <dave.visser@outlook.com>
2020-11-30 16:52:56 +0100

    upload image + add some text, still have to manually write name for save as .png

--- frontend/src/app/pages/createpage/createpage.component.ts
+++ frontend/src/app/pages/createpage/createpage.component.ts
@@ -2,42 +2,64 @@ import { Component, OnInit } from '@angular/core';
 
 function fillCanvas(){
   
-  var canvas = document.querySelector('canvas'),
-  ctx = canvas.getContext('2d');
-  var image = document.querySelector('img');
-  console.log(image);
-  canvas.width = image.width;
-  canvas.setAttribute("crossOrigin" , "Anonymous");
-  canvas.height = image.height;
-  ctx.drawImage(image, 0, 0);
-  ctx.font = "36pt Verdana";
-  console.log(canvas);
+  
   var inputField = document.querySelector('input');
-  console.log(inputField);
+  var canvas = document.querySelector('canvas')
+  var ctx = canvas.getContext('2d');
   inputField.addEventListener("keyup", function(){
+  
+   
+    var image = document.querySelector('img');
+    canvas.width = image.width;
+    canvas.setAttribute("crossOrigin" , "Anonymous");
+    canvas.height = image.height;
+    ctx.drawImage(image, 0, 0);
+    ctx.font = "64px Verdana";
      //redraw image
-     ctx.clearRect(0,0,canvas.width,canvas.height);
+     ctx.clearRect(0,0,image.width,image.height);
+     console.log(canvas.width);
      ctx.drawImage(image, 0, 0);
      //refill text
      ctx.fillStyle = "black";
      ctx.fillText(inputField.value,40,80);
   })
   
+  var imageLoader = document.getElementById('imageLoader');
+    imageLoader.addEventListener('change', handleImage, false);
+
   var savebutton = document.querySelector('.save-button');
   savebutton.addEventListener("click", function(){
+
     var savedImage = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
     window.location.href= savedImage;
   })
+}
 
-  // $('.save_button').click(function(){
-      
-  //     console.log(canvas);
-  //     var image = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");  // here is the most important part because if you dont replace you will get a DOM 18 exception.
-  //     console.log(image)
 
-  //     window.location.href=image; // it will save locally
-  // })
-  }
+// var ctx = canvas.getContext('2d');
+
+
+function handleImage(e){
+  var canvas = document.querySelector('canvas')
+  var ctx = canvas.getContext('2d');
+    var reader = new FileReader();
+    reader.onload = function(event){
+        var img = new Image();
+        img.onload = function(){
+            canvas.width = img.width;
+            canvas.height = img.height;
+            ctx.drawImage(img,0,0);
+        }
+        img.src= event.target.result as string;
+        var temp = event.target.result as string;
+        var image = document.querySelector('img');
+        image.setAttribute("src", temp)
+    }
+
+   
+    reader.readAsDataURL(e.target.files[0]);     
+}
+  
 
 
 @Component({
