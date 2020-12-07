49293bbd6bd94d6c68967625a538f187df9a5911
cjvisser <cornevisser0808@gmail.com>
2020-12-07 17:12:28 +0100

    changed API URL

--- frontend/src/app/_helpers/MemeService.ts
+++ frontend/src/app/_helpers/MemeService.ts
@@ -6,7 +6,7 @@ import { Meme } from '../shared/models/Meme';
 
 const environment = {
   production: false,
-  apiUrl: 'https://localhost:44386/api'
+  apiUrl: 'http://localhost:8080/api'
 };
 
 @Injectable()
