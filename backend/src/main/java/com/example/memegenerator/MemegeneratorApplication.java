2097910ee07c47514cc3e8aade8d7f19423b54e3
Thomas Visser <tv@zrx.nl>
2020-12-07 15:44:09 +0100

    Config added

--- backend/src/main/java/com/example/memegenerator/MemegeneratorApplication.java
+++ backend/src/main/java/com/example/memegenerator/MemegeneratorApplication.java
@@ -2,13 +2,20 @@ package com.example.memegenerator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import com.example.memegenerator.websecurity.AppProperties;
 
 @SpringBootApplication
-public class MemegeneratorApplication {
+public class MemegeneratorApplication extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MemegeneratorApplication.class);
+	}
+	
     public static void main(String[] args) {
         SpringApplication.run(MemegeneratorApplication.class, args);
     }
