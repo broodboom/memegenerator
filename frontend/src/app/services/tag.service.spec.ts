3e39407479a0dadeb5e2e846588a719410f4fb70
Andy Bhadai <abhadai@gmail.com>
2021-01-11 16:17:28 +0100

    TagService frontend tests

--- frontend/src/app/services/tag.service.spec.ts
+++ frontend/src/app/services/tag.service.spec.ts
@@ -1,16 +1,106 @@
 import { TestBed } from '@angular/core/testing';
-
+import { Tag } from 'app/models/Tag';
+import { Observable } from 'rxjs';
+import 'rxjs/add/observable/of';
+import 'rxjs/add/observable/throw';
+import { HttpClient } from '@angular/common/http';
 import { TagService } from './tag.service';
+import { ExpandOperator } from 'rxjs/internal/operators/expand';
+
+export class HttpClientStub {
+  request(): Observable<any> {
+    return Observable.of({});
+  }
+
+  get(): Observable<any> {
+    // Collection of fake tags
+    const tags: Tag[] = [];
+
+    // Create collection of fake tags
+    for (let i = 0; i < 5; i++) {
+      const tag: Tag = {
+        id: i,
+        title: `Tag ${i}`,
+      };
+
+      tags.push(tag)
+    };
+
+    return Observable.of(tags);
+  }
+
+  put(): Observable<any> {
+    return Observable.of({});
+  }
+
+  post(): Observable<any> {
+    return Observable.of({});
+  }
+}
 
 describe('TagService', () => {
   let service: TagService;
 
+  const httpClientStub = new HttpClientStub();
+
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        {
+          provide: HttpClient,
+          useValue: httpClientStub
+        }
+      ]
+    });
     service = TestBed.inject(TagService);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('should create a tag', (done) => {
+    const tag: Tag = {
+      id: 1,
+      title: 'Test tag'
+    };
+
+    spyOn(service, 'createTag').and.returnValue({});
+
+    spyOn(httpClientStub, 'post').and.callThrough();
+
+    service.createTag(tag)
+
+    expect(service.createTag).toHaveBeenCalledWith(tag);
+
+    done();
+  });
+
+  it('should get all tags', (done) => {
+
+    spyOn(httpClientStub, 'get').and.callThrough();
+
+    service.getTags().subscribe((response) => {
+
+      expect(response).not.toBeNull();
+
+      // Complete
+      done();
+    }, () => {
+      done.fail();
+    }, () => {
+      done();
+    })
+  });
+
+  it('should fail to get all tags', (done) => {
+
+    spyOn(httpClientStub, 'get').and.returnValue(Observable.throw('Error getting all tags'));
+
+    service.getTags().subscribe(() => {
+
+      done.fail()
+
+    }, (response) => {
+      done();
+    }, () => {
+      done.fail();
+    })
   });
 });
