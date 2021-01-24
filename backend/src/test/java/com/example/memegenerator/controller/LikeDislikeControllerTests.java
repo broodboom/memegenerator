db53e939e2b7920627f8becbe656c4f352be46b8
Thomas Visser <t.visser@salvux.com>
2021-01-24 20:03:22 +0100

    Build fix

--- backend/src/test/java/com/example/memegenerator/controller/LikeDislikeControllerTests.java
+++ backend/src/test/java/com/example/memegenerator/controller/LikeDislikeControllerTests.java
@@ -18,7 +18,6 @@ import com.jayway.jsonpath.JsonPath;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.WebIntegrationTest;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
